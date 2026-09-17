55f430d2a7638e110be4ca998e5a0b570f4e86df
Aryan Jaiswal <your-github-aj80529754@gmail.com>
2026-09-17 19:48:07 +0530

    Create Contact repository

--- contact-manager/src/main/java/com/contact_manager/repository/ContactRepository.java
+++ contact-manager/src/main/java/com/contact_manager/repository/ContactRepository.java
@@ -1,4 +1,8 @@
 package com.contact_manager.repository;
 
-public interface ContactRepository {
+import com.contact_manager.entity.Contact;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
 }
